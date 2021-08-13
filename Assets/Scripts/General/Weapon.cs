d974b3f44f043b2131fb77b55074c9f090b69054
jim-low <robil12380@gmail.com>
2021-08-13 11:27:05 +0800

    fix(aim): better aim function with effects

--- Assets/Scripts/General/Weapon.cs
+++ Assets/Scripts/General/Weapon.cs
@@ -4,31 +4,47 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    private Transform weapon;
+    public Transform firePoint;
+    public LineRenderer bulletLine;
 
-    public Camera cam;
-
-    Rigidbody2D rb;
-    Vector2 mousePosition;
-    Transform player;
-
-    void Start()
+    void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        player = gameObject.GetComponentInParent<Transform>();
+        weapon = transform.Find("Weapon");
     }
 
-    // Update is called once per frame
     void Update()
     {
         Aim();
+        if (Input.GetMouseButtonDown(0)) {
+            StartCoroutine(Shoot());
+        }
     }
 
     void Aim()
     {
-        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 lookDirection = mousePosition - rb.position;
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 aimDirection = (mousePos - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        weapon.eulerAngles = new Vector3(0, 0, angle);
+        /* Debug.Log(angle); */
+    }
 
-        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
-        rb.rotation = angle;
+    IEnumerator Shoot()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+
+        bulletLine.SetPosition(0, firePoint.position);
+
+        if (hitInfo) {
+            bulletLine.SetPosition(1, hitInfo.point);
+        }
+        else {
+            bulletLine.SetPosition(1, firePoint.position + firePoint.right * 100);
+        }
+
+        bulletLine.enabled = true;
+        yield return new WaitForSeconds(0.02f);
+        bulletLine.enabled = false;
     }
 }
