d27b68122b4f93e15e5641533117e5ce249116d6
jim-low <robil12380@gmail.com>
2021-08-26 08:39:57 +0800

    feat: add menu script for game main menu and settings menu

---
+++ Assets/Scripts/General/Menu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject settingsMenu;
+    public GameObject mainMenu;
+    public Slider BGMSlider;
+
+    // TODO: find out how to set game sound effect
+    public Slider soundEffectSlider;
+
+    public AudioSource music;
+
+    bool settingsOn = false;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Ground");
+    }
+
+    public void SetBGMVolume()
+    {
+        music.volume = BGMSlider.value;
+    }
+
+    public void ToggleSettings()
+    {
+        settingsOn = !settingsOn;
+
+        settingsMenu.SetActive(settingsOn);
+        mainMenu.SetActive(!settingsOn);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
