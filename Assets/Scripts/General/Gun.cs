9af070a63027b813a099a84e8d83d8c65fb06b47
jim-low <robil12380@gmail.com>
2021-08-26 09:50:49 +0800

    refactor: rework gun system into individual script

---
+++ Assets/Scripts/General/Gun.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Gun : MonoBehaviour
+{
+    private Transform firePoint;
+    private Transform weaponPosition;
+    private SpriteRenderer weaponSprite;
+
+    [SerializeField]
+    private int MAX_AMMO = 30;
+
+    public int ammo;
+    public float reloadTime = 1.25f;
+    public static TextMeshProUGUI reloadText;
+    public static TextMeshProUGUI ammoInfoText;
+    public static LineRenderer bulletLine;
+
+    bool hasAmmo = true;
+    bool needReload = false;
+
+    void Start()
+    {
+        ammo = MAX_AMMO;
+
+        weaponPosition = GetComponent<Transform>();
+        weaponSprite = GetComponentInChildren<SpriteRenderer>();
+        firePoint = GameObject.Find(gameObject.name + "FirePoint").transform;
+
+        reloadText = GameObject.Find("ReloadText").GetComponent<TextMeshProUGUI>();
+        ammoInfoText = GameObject.Find("WeaponAmmo").GetComponent<TextMeshProUGUI>();
+        bulletLine = GameObject.Find("BulletLine").GetComponent<LineRenderer>();
+
+        Debug.Log(MAX_AMMO);
+        Debug.Log(reloadText);
+        Debug.Log(ammoInfoText);
+        Debug.Log(bulletLine);
+    }
+
+    void Update()
+    {
+        Aim();
+
+        UpdateWeapon();
+
+        if (hasAmmo && Input.GetMouseButtonDown(0)) {
+            StartCoroutine(Shoot());
+        }
+
+        if (needReload || Input.GetKeyDown(KeyCode.R)) {
+            StartCoroutine(Reload());
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        if (ammo < MAX_AMMO) {
+            needReload = false;
+            reloadText.text = "Reloading...";
+            yield return new WaitForSeconds(reloadTime);
+            ammo = MAX_AMMO;
+            hasAmmo = true;
+            reloadText.text = "";
+        }
+    }
+
+    void Aim()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 aimDirection = (mousePos - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+
+        weaponSprite.flipY = Mathf.Abs(angle) > 90;
+        weaponPosition.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    IEnumerator Shoot()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+
+        bulletLine.SetPosition(0, firePoint.position);
+
+        Vector2 bulletDestination = hitInfo ? (Vector2)hitInfo.point : (Vector2)(firePoint.position + firePoint.right * 100);
+        bulletLine.SetPosition(1, bulletDestination);
+
+        bulletLine.enabled = true;
+        yield return new WaitForSeconds(0.02f);
+        bulletLine.enabled = false;
+
+        --ammo;
+    }
+
+    void UpdateWeapon()
+    {
+        if (ammo <= 0) {
+            needReload = true;
+            hasAmmo = false;
+        }
+
+        ammoInfoText.text = ammo + " / " + MAX_AMMO;
+    }
+}
