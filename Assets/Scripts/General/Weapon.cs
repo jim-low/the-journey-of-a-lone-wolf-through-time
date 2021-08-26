e57ce9dee6207535021fe8dd8659d386a016e9ea
jim-low <robil12380@gmail.com>
2021-08-26 09:51:34 +0800

    chore: remove gun logic into ./Gun.cs

--- Assets/Scripts/General/Weapon.cs
+++ Assets/Scripts/General/Weapon.cs
@@ -5,92 +5,29 @@ using TMPro;
 
 public class Weapon : MonoBehaviour
 {
-    public int ammo = 30;
-    public float reloadTime = 2f;
-
-    public Transform firePoint;
-    public LineRenderer bulletLine;
-    public TextMeshProUGUI reloadText;
-    public TextMeshProUGUI ammoText;
-
-    private Transform weaponPosition;
-    private SpriteRenderer weaponSprite;
-
-    const int MAX_AMMO = 30;
-    bool hasAmmo = true;
-    bool needReload = false;
+    GameObject rifle;
+    GameObject pistol;
 
     void Awake()
     {
-        GameObject rifle = GameObject.Find("Rifle");
-        weaponPosition = rifle.GetComponent<Transform>();
-        weaponSprite = rifle.GetComponentInChildren<SpriteRenderer>();
+        rifle = GameObject.Find("Rifle");
+        pistol = GameObject.Find("Pistol");
+        pistol.SetActive(false);
     }
 
     void Update()
     {
-        Aim();
-
-        UpdateWeapon();
-
-        if (hasAmmo && Input.GetMouseButtonDown(0)) {
-            StartCoroutine(Shoot());
-        }
-
-        if (needReload || Input.GetKeyDown(KeyCode.R)) {
-            StartCoroutine(Reload());
+        if (Input.GetKeyDown(KeyCode.F)) {
+            SwitchGun();
         }
     }
 
-    IEnumerator Reload()
+    void SwitchGun()
     {
-        if (ammo < MAX_AMMO) {
-            needReload = false;
-            reloadText.text = "Reloading...";
-            yield return new WaitForSeconds(reloadTime);
-            ammo = MAX_AMMO;
-            hasAmmo = true;
-            reloadText.text = "";
-        }
-    }
+        // negate current rifle active value
+        bool rifleActive = !rifle.activeInHierarchy;
 
-    void Aim()
-    {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 aimDirection = (mousePos - transform.position).normalized;
-        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
-
-        weaponSprite.flipY = Mathf.Abs(angle) > 90;
-        weaponPosition.eulerAngles = new Vector3(0, 0, angle);
-    }
-
-    IEnumerator Shoot()
-    {
-        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
-
-        bulletLine.SetPosition(0, firePoint.position);
-
-        if (hitInfo) {
-            bulletLine.SetPosition(1, hitInfo.point);
-        }
-        else {
-            bulletLine.SetPosition(1, firePoint.position + firePoint.right * 100);
-        }
-
-        bulletLine.enabled = true;
-        yield return new WaitForSeconds(0.02f);
-        bulletLine.enabled = false;
-
-        --ammo;
-    }
-
-    void UpdateWeapon()
-    {
-        if (ammo <= 0) {
-            needReload = true;
-            hasAmmo = false;
-        }
-
-        ammoText.text = ammo + " / " + MAX_AMMO;
+        rifle.SetActive(rifleActive);
+        pistol.SetActive(!rifleActive);
     }
 }
