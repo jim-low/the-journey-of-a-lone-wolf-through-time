ab0ddcadaada1b6b99fc3405e2329e0e6d16d14f
jim-low <robil12380@gmail.com>
2021-09-09 20:41:39 +0800

    feat: add base turret AI

---
+++ Assets/Scripts/AI/Turret.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    private Transform firePoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
